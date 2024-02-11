34e1c03215dcd2a9ac06a0ca9b77ab9e8760c46f
Guilherme Santos <contatoguilhermecsantos@gmail.com>
2024-02-11 17:48:15 -0300

    Tarefa Para Calcular a média

---
+++ mod8/TarefaCalculoMedia/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double nota1 = 5.0;
+        double nota2 = 10.0;
+        double nota3 = 15.0;
+        double nota4 = 20.0;
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+        System.out.print(media);
+
+    }
+}
