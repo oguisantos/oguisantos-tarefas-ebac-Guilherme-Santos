eb2b89730f888cc592df60fece01093cbfcbf658
Guilherme Santos <contatoguilhermecsantos@gmail.com>
2024-02-11 21:27:21 -0300

    Tarefa Wrapper

---
+++ mod9/Tarefa_Wrapper/src/br/com/gsantos/Main.java
@@ -0,0 +1,17 @@
+package br.com.gsantos;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double valor1 = 20d;
+		
+		System.out.println(valor1);
+		
+		Double valor2 = Double.valueOf(valor1);
+		
+		System.out.println(valor2);
+
+	}
+
+}
